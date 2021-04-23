6f241b977bf1d457d00385abb4e4a7ee27aec452
Tyler Hostler-Mathis <tylerhm.dev@gmail.com>
2021-04-23 15:03:43 -0400

    Modularized

---
+++ src/components/AboutMe.js
@@ -0,0 +1,24 @@
+import aboutMe from '../images/about-me.webp'
+import './AboutMe.css'
+
+const AboutMe = () => {
+  return (
+    <div className='about-me'>
+      <img src={aboutMe} alt='Headshot About-Me' />
+      <div className='flex-vertical justify-content-between'>
+        <p className='header'>ABOUT ME!</p>
+        <p className='content'>
+          I am a twenty year old actor/singer/dancer originally from Tampa, Florida.
+          I developed my love for musical theatre at the age of 5, listening to the Wicked soundtrack on repeat.
+          I am currently pursuing a BFA in Musical Theatre at Boston Conservatory with a minor in Psychology.
+          When I'm not studying I enjoy trying new foods, dabbling in photography, and doing yoga.
+        </p>
+        <button className='button' onClick={() => {}}>
+          See Resume
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default AboutMe
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import {
   Background
 } from 'react-parallax'
 import background from '../images/background.webp'
-import aboutMe from '../images/about-me.webp'
+import AboutMe from '../components/AboutMe'
 import './Home.css'
 
 const Home = () => {
@@ -21,20 +21,9 @@ const Home = () => {
           <p className='header sub'>Actor & Model</p>
         </div>
       </Parallax>
-      <div className='about-me'>
-        <img src={aboutMe} alt='Headshot About-Me' />
-        <div className='flex-vertical justify-content-between'>
-          <p className='header'>ABOUT ME!</p>
-          <p className='content'>
-            I am a twenty year old actor/singer/dancer originally from Tampa, Florida.
-            I developed my love for musical theatre at the age of 5, listening to the Wicked soundtrack on repeat.
-            I am currently pursuing a BFA in Musical Theatre at Boston Conservatory with a minor in Psychology.
-            When I'm not studying I enjoy trying new foods, dabbling in photography, and doing yoga.
-          </p>
-          <button className='button' onClick={() => {}}>
-            See Resume
-          </button>
-        </div>
+      <AboutMe />
+      <div className='contact-me'>
+
       </div>
     </div>
   )
