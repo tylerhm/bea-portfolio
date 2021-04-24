6d16348c433aaea4f8b6585cbebebb0eecbc66fe
Tyler Hostler-Mathis <tylerhm.dev@gmail.com>
2021-04-24 03:05:24 -0400

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,21 @@
+import {
+  FaGithub,
+  FaLinkedin,
+} from 'react-icons/fa'
+import './Footer.scss'
+
 const Footer = () => {
   return (
-    <div>
-
+    <div className='footer'>
+      <div className='copyright'>© 2021 by Tyler Hostler-Mathis.</div>
+      <div className='socials'>
+        <a href='https://www.github.com/TylerMathis' target='__blank'>
+          <FaGithub />
+        </a>
+        <a href='https://www.linkedin.com/in/TylerHostlerMathis' target='__blank'>
+          <FaLinkedin />
+        </a>
+      </div>
     </div>
   )
 }
