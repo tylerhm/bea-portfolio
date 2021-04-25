19a1e5d951f74aabd418b0b7406cbaa1f06ee073
Tyler Hostler-Mathis <tylerhm.dev@gmail.com>
2021-04-25 19:31:57 -0400

    Changed middle image

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -6,8 +6,8 @@ const Gallery = () => {
       <a href='https://imgur.com/0KHxC7g' target='__blank' className='container'>
         <img src={'https://i.imgur.com/0KHxC7gh.jpg'} alt='Gallery Headshot One'></img>
       </a>
-      <a href='https://imgur.com/G4vlvNX' target='__blank' className='container'>
-        <img src={'https://i.imgur.com/G4vlvNXh.jpg'} alt='Gallery Headshot Two'></img>
+      <a href='https://imgur.com/sSHCzyY' target='__blank' className='container'>
+        <img src={'https://i.imgur.com/sSHCzyYh.png'} alt='Gallery Headshot Two'></img>
       </a>
       <a href='https://imgur.com/6OJ7Jkc' target='__blank' className='container'>
         <img src={'https://i.imgur.com/6OJ7Jkch.jpg'} alt='Gallery Headshot Three'></img>
