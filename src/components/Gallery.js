aac6564bab39877266b9592a78bd9f233ca2785a
Tyler Hostler-Mathis <tylerhm.dev@gmail.com>
2021-04-28 15:41:44 -0400

    Changed left image

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -3,8 +3,8 @@ import './Gallery.scss'
 const Gallery = () => {
   return (
     <div className='gallery'>
-      <a href='https://imgur.com/0KHxC7g' target='__blank' className='container'>
-        <img src={'https://i.imgur.com/0KHxC7gh.jpg'} alt='Gallery Headshot One'></img>
+      <a href='https://imgur.com/boF4s0N' target='__blank' className='container'>
+        <img src={'https://i.imgur.com/boF4s0Nh.jpg'} alt='Gallery Headshot One'></img>
       </a>
       <a href='https://imgur.com/sSHCzyY' target='__blank' className='container'>
         <img src={'https://i.imgur.com/sSHCzyYh.png'} alt='Gallery Headshot Two'></img>
