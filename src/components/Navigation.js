0e8898867bd205828a9fac8b36a9cb4cc8fc1af1
Tyler Hostler-Mathis <tylerhm.dev@gmail.com>
2021-08-13 09:53:54 -0400

    Close menu on select

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,22 +1,18 @@
-import {
-  Nav,
-  Navbar
-}
-from 'react-bootstrap';
+import { Nav, Navbar } from 'react-bootstrap'
 import { NavLink } from 'react-router-dom'
-import {
-  FaFacebookF,
-  FaInstagram
-} from 'react-icons/fa'
+import { FaFacebookF, FaInstagram } from 'react-icons/fa'
+import { useState } from 'react'
 import PagesIndex from '../pages/PagesIndex'
 import './Navigation.scss'
 
 const Navigation = () => {
+  const [expanded, setExpanded] = useState(false)
+  const collapse = () => setExpanded(false)
 
   const pages = []
   for (const [key, value] of Object.entries(PagesIndex)) {
     pages.push(
-      <Nav.Item key={key}>
+      <Nav.Item key={key} onClick={collapse}>
         <NavLink activeClassName='active' to={value}>
           {key}
         </NavLink>
@@ -25,20 +21,34 @@ const Navigation = () => {
   }
 
   return (
-    <Navbar className='py-3' fixed='top' expand='md'>
+    <Navbar
+      className='py-3'
+      fixed='top'
+      expand='md'
+      expanded={expanded}
+      onToggle={(newExpanded) => setExpanded(newExpanded)}
+    >
       <div className='social-group'>
-        <Nav.Link href='https://www.facebook.com/profile.php?id=100016575947941' target='_blank' rel='noreferrer'>
+        <Nav.Link
+          href='https://www.facebook.com/profile.php?id=100016575947941'
+          target='_blank'
+          rel='noreferrer'
+          onClick={collapse}
+        >
           <FaFacebookF />
         </Nav.Link>
-        <Nav.Link href='https://www.instagram.com/beariverairions' target='_blank' rel='noreferrer'>
+        <Nav.Link
+          href='https://www.instagram.com/briverairions'
+          target='_blank'
+          rel='noreferrer'
+          onClick={collapse}
+        >
           <FaInstagram />
         </Nav.Link>
       </div>
       <Navbar.Toggle />
       <Navbar.Collapse>
-        <Nav>
-          {pages}
-        </Nav>
+        <Nav>{pages}</Nav>
       </Navbar.Collapse>
     </Navbar>
   )
