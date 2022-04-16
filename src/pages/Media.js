c4f1cec0103c099b952089cd79ed9590fd3f52b4
Tyler Hostler-Mathis <tylerhm.dev@gmail.com>
2022-04-16 11:26:21 -0400

    New vids

--- src/pages/Media.js
+++ src/pages/Media.js
@@ -9,11 +9,21 @@ const Media = () => {
 
   return (
     <div className='media'>
-      <div>
+      <div className='video-container'>
         <iframe
           className='video'
-          src='https://www.youtube.com/embed/CGjRPwctxIU'
-          title='YouTube video player'
+          src='https://www.youtube.com/embed/h1Rb0oOJsv8'
+          title='Voice Reel'
+          frameborder='0'
+          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+          allowfullscreen
+        />
+      </div>
+      <div className='video-container'>
+        <iframe
+          className='video'
+          src='https://www.youtube.com/embed/0coXqufsFM4'
+          title='Dance Reel'
           frameborder='0'
           allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
           allowfullscreen
