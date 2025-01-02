c485b5e5db26380e3e3e03ed4c748f484346342d
Tyler Hostler-Mathis <tylerhm.dev@gmail.com>
2025-01-01 20:18:33 -0500

    banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
+import "./App.css";
+
 const App = () => {
   return (
     <Router>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,42 @@
-const Home = () => {
-  return <div>Hello</div>;
+import { Parallax, Background } from "react-parallax";
+import banner from "../images/banner.jpg";
+
+const ParallaxBanner = () => {
+  return (
+    <Parallax strength={250}>
+      <Background>
+        <img
+          style={{
+            width: "100vw",
+            height: "800px",
+            objectFit: "cover",
+            objectPosition: "0% 18%",
+          }}
+          src={banner}
+          alt="Headshot"
+        />
+      </Background>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          textAlign: "center",
+          height: "800px",
+          color: "white",
+          fontSize: "5em",
+          fontWeight: "bold",
+        }}
+      >
+        <p>
+          BIANCA
+          <br />
+          RIVERA-IRIONS
+        </p>
+      </div>
+    </Parallax>
+  );
 };
 
-export default Home;
+export default ParallaxBanner;
