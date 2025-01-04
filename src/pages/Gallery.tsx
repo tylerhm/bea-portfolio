5c221477e706cf473a384e45de5cb50c65d0ed68
Tyler Hostler-Mathis <tylerhm.dev@gmail.com>
2025-01-04 17:04:59 -0500

    tmp gallery

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -1,9 +1,38 @@
 import Nav from "../components/Nav";
 
+const images = [
+  "https://i.imgur.com/boF4s0Nh.jpg",
+  "https://i.imgur.com/sSHCzyYh.png",
+  "https://i.imgur.com/6OJ7Jkch.jpg",
+  "https://i.imgur.com/6OJ7Jkch.jpg",
+  "https://i.imgur.com/boF4s0Nh.jpg",
+  "https://i.imgur.com/sSHCzyYh.png",
+  "https://i.imgur.com/sSHCzyYh.png",
+  "https://i.imgur.com/6OJ7Jkch.jpg",
+  "https://i.imgur.com/boF4s0Nh.jpg",
+];
+
 const Gallery = () => {
   return (
-    <div style={{ backgroundColor: "black", height: "100vh", width: "100vw" }}>
+    <div
+      style={{
+        width: "100vw",
+        display: "flex",
+        flexDirection: "row",
+        flexWrap: "wrap",
+      }}
+    >
       <Nav />
+      {images.map((image) => {
+        return (
+          <img
+            style={{
+              width: "33.33333vw",
+            }}
+            src={image}
+          />
+        );
+      })}
     </div>
   );
 };
